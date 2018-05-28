374a4f784f0314aa49678d55df82e19d693ea3d3
erika.tokumoto <>
2018-05-28 06:25:44 +0000

    CRUD tasks

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -2,6 +2,12 @@
 
 @section('content')
 
+<h1>新規タスク作成ページ</h1>
+{!! Form::model($task, ['route' => 'tasks.store']) !!}
+        {!! Form::label('content', 'タスク:') !!}
+        {!! Form::text('content') !!}
 
+        {!! Form::submit('投稿') !!}
 
+    {!! Form::close() !!}
 @endsection
