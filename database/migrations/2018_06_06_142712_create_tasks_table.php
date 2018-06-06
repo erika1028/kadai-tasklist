3d425fb6c79afa6158dfe3907c4515d8aaa5c75d
erika.tokumoto <>
2018-06-06 05:33:29 +0000

    user login

--- database/migrations/2018_06_06_142712_create_tasks_table.php
+++ database/migrations/2018_06_06_142712_create_tasks_table.php
@@ -11,12 +11,18 @@ class CreateTasksTable extends Migration
      *
      * @return void
      */
-    public function up()
+ public function up()
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('content');  
+            $table->integer('user_id')->unsigned()->index();
+            $table->string('content');
+            $table->string('status',10);
             $table->timestamps();
+            
+
+            // Foreign key constraint
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
